dbb77e42461ed49c57d64c37353d854d3e7bc64f
nhardbalansag <nhardbalansag@gmail.com>
2021-07-14 09:10:04 +0800

    remove test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -8,13 +8,6 @@ class TestController extends Controller
 {
     public function test(){
 
-        $response = \GoogleMaps::load('directions')
-            ->setParam([
-                'origin'          => 'place_id:ChIJ685WIFYViEgRHlHvBbiD5nE',
-                'destination'     => 'place_id:ChIJA01I-8YVhkgRGJb0fW4UX7Y',
-            ])
-           ->isLocationOnEdge(55.86483,-4.25161);
-
-        dd($response);
+        dd("helllo");
     }
 }
